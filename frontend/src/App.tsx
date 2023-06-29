04b9e4ca9245ce5c868c9463b6997b282c495509
random56428 <55150488+random56428@users.noreply.github.com>
2023-06-29 14:34:42 -0700

    implemented sidebar with animation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import './App.css'
 import RecipeForm from './components/recipe/RecipeForm.tsx'
 import Header from './components/Header.tsx'
+import Sidebar from './components/Sidebar.tsx'
 
 function App() {
 
   return (
     <>
       <Header />
-      <RecipeForm />
+      {/* <RecipeForm /> */}
+      <Sidebar />
     </>
   )
 }
