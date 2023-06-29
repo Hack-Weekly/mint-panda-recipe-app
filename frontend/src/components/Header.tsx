7671c7126b5b88a92fef65a4c98023e6cc3eccc1
random56428 <55150488+random56428@users.noreply.github.com>
2023-06-29 03:03:37 -0700

    update tailwindcss to work with vite and implemented header

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="bg-[#41482e] py-6 w-[100%] m-0">
+      <div className="container mx-auto text-center">
+        <h1 className="text-2xl text-white">Mint Panda</h1>
+      </div>
+    </header>
+  )
+}
+
+export default Header
