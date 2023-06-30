14089f0af82716cc198694dd27d6ac282c077220
random56428 <55150488+random56428@users.noreply.github.com>
2023-06-30 16:46:45 -0700

    initialize api template

---
+++ frontend/src/util/api.tsx
@@ -0,0 +1,17 @@
+const getRecipes = async () => {
+  return null // TODO
+}
+
+const getRecipe = async (uuid) => {
+  return null // TODO
+}
+
+const updateRecipe = async (recipe) => {
+  return null // TODO
+}
+
+export {
+  getRecipes,
+  getRecipe,
+  updateRecipe
+}
