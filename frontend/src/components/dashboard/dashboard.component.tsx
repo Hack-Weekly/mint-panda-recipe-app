12de0911e77300794e50363d19d010bbc318a412
jcastillo21 <bcastillo-2022474@kinal.edu.gt>
2023-06-29 23:02:12 -0600

    dashboard finished

--- frontend/src/components/dashboard/dashboard.component.tsx
+++ frontend/src/components/dashboard/dashboard.component.tsx
@@ -1,43 +1,27 @@
 import "./dahsboard.component.scss"
 import RecipeComponent from "../recipe/recipe.component.tsx";
+import SidebarComponent from "../sidebar/sidebar.component.tsx";
 
 const DashboardComponent = () => {
     return (
         <div className="grid-container">
-            <div className="header fs-5 text-white padding-y-2">
+            <h1 className="header text-white py-10 text-5xl font-bold ">
                 Mint panda
+            </h1>
+            <div className="sidebar">
+                <SidebarComponent></SidebarComponent>
             </div>
-            <div className="sidebar"></div>
             <div className="main padding-5 ">
-                <h2 className="h4 border-bottom border-weight-medium">
+                <h2 className="h4 border-b-4 border-b-black">
                     today's feature
                 </h2>
-                <div className="recipes-container">
-                    <div className="recipes-row">
-                        <RecipeComponent></RecipeComponent>
-                        <RecipeComponent></RecipeComponent>
-                        <RecipeComponent></RecipeComponent>
-                    </div>
-                    <div className="recipes-row">
-                        <RecipeComponent></RecipeComponent>
-                        <RecipeComponent></RecipeComponent>
-                        <RecipeComponent></RecipeComponent>
-                    </div>
-                    <div className="recipes-row">
-                        <RecipeComponent></RecipeComponent>
-                        <RecipeComponent></RecipeComponent>
-                        <RecipeComponent></RecipeComponent>
-                    </div>
-                    <div className="recipes-row">
-                        <RecipeComponent></RecipeComponent>
-                        <RecipeComponent></RecipeComponent>
-                        <RecipeComponent></RecipeComponent>
-                    </div>
-                    <div className="recipes-row">
-                        <RecipeComponent></RecipeComponent>
-                        <RecipeComponent></RecipeComponent>
-                        <RecipeComponent></RecipeComponent>
-                    </div>
+                <div className="flex flex-wrap gap-x-16 p-12 justify-around gap-y-16">
+                    <RecipeComponent></RecipeComponent>
+                    <RecipeComponent></RecipeComponent>
+                    <RecipeComponent></RecipeComponent>
+                    <RecipeComponent></RecipeComponent>
+                    <RecipeComponent></RecipeComponent>
+                    <RecipeComponent></RecipeComponent>
                 </div>
             </div>
         </div>
--- frontend/src/components/recipe/recipe.component.tsx
+++ frontend/src/components/recipe/recipe.component.tsx
@@ -1,19 +1,22 @@
 import image from "../../assets/images/image-placeholder.png"
 import "./recipe.component.scss"
-
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faClock, faHeart, faPlateWheat} from "@fortawesome/free-solid-svg-icons";
 // const RecipeComponent = ({name: string, ingredients: {name: string, amount: string}[], instructions: string, description: string}) => {
 const RecipeComponent = () => {
     return (
-        <div className="p-2 recipe border">
+        <div className="rounded-3xl drop-shadow-xl p-5 hover:cursor-pointer" style={{backgroundColor: "#ebf5e6"}}>
             <img className="recipe__image" src={image} alt="some image"/>
-            <div className="recipe__name">some name</div>
-            <div className="d-flex gap-1">
-                <div className="icon">icon</div>
-                <div className="quantity">4 servings</div>
+            <div className="font-bold text-xl">some name</div>
+            <div className="flex gap-1">
+                <div><FontAwesomeIcon icon={faPlateWheat}></FontAwesomeIcon></div>
+                <div className="text-xs">4 servings</div>
             </div>
-            <div className="margin-top-3 icons">
-                <div className="heart-icon">icon</div>
-                <div className="time-icon">icon</div>
+            <div className="mt-10 icons">
+                <div className="flex gap-2 items-center"><FontAwesomeIcon
+                    icon={faHeart}></FontAwesomeIcon><span>70</span></div>
+                <div className="flex gap-2 items-center"><FontAwesomeIcon
+                    icon={faClock}></FontAwesomeIcon><span>35 min</span></div>
             </div>
         </div>
     )
