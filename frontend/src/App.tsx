6c051f90a59b0994c1bdbfcb8eb2e49bf3d54144
Nathan-CSE <86046189+Nathan-CSE@users.noreply.github.com>
2023-07-02 02:22:32 +1000

    finished single recipe page, added tags and added more coherent placeholder text

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,12 @@ import RecipeForm from './components/recipe/RecipeForm.tsx'
 import Header from './components/Header.tsx'
 import Sidebar from './components/Sidebar.tsx'
 import SingleRecipe from './components/single-recipe/SingleRecipe.tsx'
+import tags from './components/single-recipe/tags.tsx'
 
 function App() {
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       {/* <RecipeForm/> */}
       {/* <Sidebar /> */}
       <SingleRecipe/>
--- frontend/src/components/single-recipe/SingleRecipe.tsx
+++ frontend/src/components/single-recipe/SingleRecipe.tsx
@@ -1,68 +1,100 @@
-import React from 'react'
 import image from "../../assets/images/image-placeholder.png"
 
 export default function SingleRecipe() {
   return (
-    // This creates a 'div' element or section which is a container for other
-    // HTML elements -> Here I have the main title in the container  
-    // mx-auto means to automatically align the margins to the center, my means margin
-    // in the y-axis and w-9/12 means the width is 9/12 of the page size
-
-    // The best way to approach image and text is to have two div elements one for image and the 
-    // other for the text, align them separately
-
-    // Container for the entire form
+    
     <div className="mx-auto my-2.5 w-9/12">
-      <img className="recipe_image float-right scale rounded-3xl w-4/12 px-2" src={image} alt="recipe"></img>
-
-      {/* Container for the header + horizontal bar */}
-      <div className="text-left w-8/12 pr-5 pb-4">
-        <h1 className="text-3xl font-bold mt-4 mb-2">SingleRecipe</h1> 
-        <div className="border-t-2 border-gray-950 mb-2"></div>
-
-        <h2 className="text-xl text-left font-bold">Tags</h2>
-        <p>
-          Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
-          Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
-          Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
-          Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
-          Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
-          Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
-          Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
-        </p>
-
-        <br></br>
-        <br></br>
-        <br></br>
-
-        
+      <div>
+        <button className="justify-start text-l">&lt;&lt; Go Back</button>
       </div>
 
-      <div className="text-left w-8/12 pr-5 pb-4">
+      <img className="float-right rounded-3xl w-1/4 p-2 h-auto" src={image} alt="recipe"></img>
+
+      <div className="text-left w-9/12 pr-5 pb-4 justify-start">
+        <h1 className="text-3xl font-bold mt-4 mb-2">Italian Wedding Soup</h1> 
+        <div className="border-t-2 border-black mb-2"></div>
+
+        <h2 className="text-xl text-left font-bold mb-2">Tags</h2>
+
+        <div className="flex justify-start space-x-2">
+          <div className="bg-gray-700 rounded px-2">
+            <p className="text-white font-semibold">#Fun</p>
+          </div>
+
+          <div className="bg-gray-700 rounded px-2">
+            <p className="text-white font-semibold">#Quick</p>
+          </div>
+
+          <div className="bg-green-700 rounded px-2 pb-1">
+            <p className="text-white font-semibold">#Vegan</p>
+          </div>
+
+        </div>
+
+        <br></br>
+
+        <h2 className="text-xl text-left font-bold mb-1">Ingredients</h2>
+
+        <div className="pl-5">
+          <ul className="list-disc">
+            <li className="mb-1">
+              1/2 lb Ground beef
+            </li>
+
+            <li className="mb-1">
+              1/2 lb Ground veal
+            </li>
+
+            <li className="mb-1">
+              1/4 c Italian seasoned bread crumb
+            </li>
+            
+            <li className="mb-1">
+              1 Egg
+            </li>
+          </ul>
+        </div>
+
+        <br></br>
+        <br></br>
+        <br></br>
+
+      </div>
+
+      <div className="text-left pr-5 pb-4">
         <h2 className="text-2xl text-left font-bold mb-2">Steps</h2>
-        <div className="border-t-2 border-gray-950"></div>
+        <div className="border-t-2 border-black"></div>
       </div>
 
       <div className="pl-5">
         <ol className="list-decimal">
           <li className="mb-2">
-            Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
-            Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
-            Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
+            Combine the ground meat, bread crumbs, egg, parsley, salt and pepper in a bowl. 
+          </li>
+    
+          <li className="mb-2">
+            Mix well and form into tiny meat balls. 
           </li>
 
           <li className="mb-2">
-            Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
-            Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
-            Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
+            Bake on a cookie sheet for 30 minutes at 350F. 
+          </li>
+
+          <li className="mb-2">
+            Meanwhile, bring broth to a boil and add spinach. 
           </li>
           
           <li className="mb-2">
-            Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
-            Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
-            Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
+            Cover and boil for 5 minutes. 
           </li>
 
+          <li className="mb-2">
+            Add the meatballs to the hot broth, bring to a simmer. 
+          </li>
+          
+          <li className="mb-2">
+            Stir in the cheese and serve immediately.
+          </li>
         </ol>
       </div>
 
