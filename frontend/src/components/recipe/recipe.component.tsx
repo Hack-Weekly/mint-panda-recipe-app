d7cc290f46aa2d45ceb2f6aa4513972ab923f0af
jcastillo21 <bcastillo-2022474@kinal.edu.gt>
2023-06-30 22:59:34 -0600

    some changes to recipes

--- frontend/src/components/recipe/recipe.component.tsx
+++ frontend/src/components/recipe/recipe.component.tsx
@@ -1,25 +1,38 @@
-import image from "../../assets/images/image-placeholder.png"
-import "./recipe.component.scss"
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faClock, faHeart, faPlateWheat} from "@fortawesome/free-solid-svg-icons";
+import image from "../../assets/images/image-placeholder.png";
+import "./recipe.component.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faClock,
+    faHeart,
+    faPlateWheat,
+} from "@fortawesome/free-solid-svg-icons";
 // const RecipeComponent = ({name: string, ingredients: {name: string, amount: string}[], instructions: string, description: string}) => {
 const RecipeComponent = () => {
     return (
-        <div className="rounded-3xl drop-shadow-xl p-5 hover:cursor-pointer" style={{backgroundColor: "#ebf5e6"}}>
-            <img className="recipe__image" src={image} alt="some image"/>
+        <div
+            className="rounded-3xl drop-shadow-xl p-5 hover:cursor-pointer"
+            style={{ backgroundColor: "#ebf5e6" }}
+        >
+            <img className="recipe__image" src={image} alt="some image" />
             <div className="font-bold text-xl">some name</div>
             <div className="flex gap-1">
-                <div><FontAwesomeIcon icon={faPlateWheat}></FontAwesomeIcon></div>
+                <div>
+                    <FontAwesomeIcon icon={faPlateWheat}></FontAwesomeIcon>
+                </div>
                 <div className="text-xs">4 servings</div>
             </div>
-            <div className="mt-10 icons">
-                <div className="flex gap-2 items-center"><FontAwesomeIcon
-                    icon={faHeart}></FontAwesomeIcon><span>70</span></div>
-                <div className="flex gap-2 items-center"><FontAwesomeIcon
-                    icon={faClock}></FontAwesomeIcon><span>35 min</span></div>
+            <div className="mt-10 flex justify-between">
+                <div className="flex gap-1 items-center">
+                    <FontAwesomeIcon icon={faHeart}></FontAwesomeIcon>
+                    <span>70</span>
+                </div>
+                <div className="flex gap-1 items-center">
+                    <FontAwesomeIcon icon={faClock}></FontAwesomeIcon>
+                    <span>35 min</span>
+                </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default RecipeComponent
+export default RecipeComponent;
