4a67ea05447fd5bc92ad7c79fe0ff7e4ace7c64f
random56428 <55150488+random56428@users.noreply.github.com>
2023-07-01 03:43:44 -0700

    adjust z-index for sidebar button

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -3,7 +3,7 @@ import Header from "./Header"
 const Navigation = () => {
   return (
     <>
-      <div className="relative z-20">
+      <div className="relative z-30">
         <Header />
       </div>
     </>
