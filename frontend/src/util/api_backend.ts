3c24d4b433e9b37c57531731450558ac0d84feec
random56428 <55150488+random56428@users.noreply.github.com>
2023-07-02 13:12:27 -0700

    return dummy data if backend not set up

--- frontend/src/util/api_backend.ts
+++ frontend/src/util/api_backend.ts
@@ -1,20 +1,27 @@
 import axios from "axios"
-axios.defaults.withCredentials = true;
+import { dummyRecipe, dummyRecipeList } from "./dummy"
 
 const API_ROOT = "http://localhost:5000"
 
 export const getRecipes = async () => {
-  const response = await axios.get(`${API_ROOT}/api/recipes`, {
+  return axios.get(`${API_ROOT}/api/recipes`, {
     withCredentials: true
   })
-  return response.data
+    .then(response => response.data)
+    .catch(error => {
+      console.log(error)
+      return dummyRecipeList
+    })
 }
 
 export const getRecipe = async (id: string) => {
-  const response = await axios.get(`${API_ROOT}/api/recipes/${id}`, {
+  return axios.get(`${API_ROOT}/api/recipes/${id}`, {
     withCredentials: true
-  })
-  return response.data
+  }).then(response => response.data)
+    .catch(error => {
+      console.log(error)
+      return dummyRecipe
+    })
 }
 
 export const updateRecipe = async () => {
