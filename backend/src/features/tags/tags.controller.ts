e9d3c4c9e15c72cfad5e1452e4112cc1b8c6b164
Ronald Ng <ronaldn1936@gmail.com>
2023-07-02 02:05:21 -0400

    tags endpoint

---
+++ backend/src/features/tags/tags.controller.ts
@@ -0,0 +1,58 @@
+import { Request, Response } from "express";
+import * as tagsRepo from './tags.repository'
+import { ReasonPhrases, StatusCodes } from "http-status-codes";
+
+export const getTags = async(req: Request, res: Response) => {
+    const results = await tagsRepo.getTags();
+
+    res
+        .status(StatusCodes.OK)
+        .send(results);
+};
+
+export const getTagsById = async(req: Request, res: Response) => {
+    const id = req.params.id ;
+    const result = await tagsRepo.getTagById(id);
+
+    if(!result) {
+        res
+            .status(StatusCodes.NOT_FOUND)
+            .send({message : `Ingredients with Id '${id}' was not found.`})
+    } else {
+        res
+        .status(StatusCodes.OK)
+        .send(result);
+    }
+}
+
+export const addTag = async(req: Request, res: Response) => {
+    const data = req.body;
+    const result = await tagsRepo.addTag(data);
+
+    if (!result) {
+        res
+            .status(StatusCodes.BAD_REQUEST)
+            .send({message: 'Tag already exists.'});
+    } else {
+        res
+            .status(StatusCodes.CREATED)
+            .send({
+                status: StatusCodes.CREATED,
+                message: `Created with Id: '${result.id}'`
+            });
+    }
+}
+
+export const deleteTag = async(req: Request, res: Response) => {
+    const id = req.params.id;
+    const result = await tagsRepo.deleteTagById(id);
+
+    if(!result)
+        res
+            .status(StatusCodes.NOT_FOUND)
+            .send({message : `Ingredients with Id '${id}' was not found.`})
+
+    res
+        .status(StatusCodes.NO_CONTENT)
+        .send(ReasonPhrases.NO_CONTENT);
+}
