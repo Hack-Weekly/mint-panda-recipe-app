aa1fca460d99525560663dfddeccd9e8f613fb05
random56428 <55150488+random56428@users.noreply.github.com>
2023-07-03 17:02:49 -0700

    show error message based on class-validator

--- frontend/src/pages/Error.tsx
+++ frontend/src/pages/Error.tsx
@@ -4,11 +4,18 @@ import { useRouteError, isRouteErrorResponse } from 'react-router-dom'
 const ErrorPage: React.FC = () => {
   const error = useRouteError()
   let errorMessage: string;
-
+  
+  console.log(error)
   if (isRouteErrorResponse(error)) {
     errorMessage = error.error?.message || error.statusText
   } else if (error instanceof AxiosError) {
-    errorMessage = error.response?.data.message || error.response?.data.status
+    if (typeof error.response?.data.message === "string" || typeof error.response?.data.status === "string") {
+      errorMessage = error.response?.data.message || error.response?.data.status
+    } else if (Array.isArray(error.response?.data.message) && error.response?.data.message[0] && error.response?.data.message[0].constraints) {
+      errorMessage = Object.values(error.response?.data.message[0].constraints).toString()
+    } else {
+      errorMessage = "Unknown error message"
+    }
   } else {
     console.log(error)
     errorMessage = "Unknown error message"
