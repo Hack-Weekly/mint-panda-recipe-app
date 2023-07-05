4d94d881b012df8f3feefb5e55b4d8034c4575da
random56428 <55150488+random56428@users.noreply.github.com>
2023-07-05 15:34:31 -0700

    changed api route

--- frontend/src/util/api_backend.ts
+++ frontend/src/util/api_backend.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 import { dummyRecipe, dummyRecipeList, dummyIngredientList, dummyTags } from "./dummy"
 import { RecipePost } from "../interfaces"
 
-const API_ROOT = "http://localhost:5000"
+const API_ROOT = "https://mint-panda-recipe-api.onrender.com"
 
 export const getRecipes = async (page = 1) => {
   return axios.get(`${API_ROOT}/api/recipes?page=${page}&orderBy=created_at&orderByPrecedence=desc`, {
@@ -30,11 +30,6 @@ export const saveRecipe = async (recipe: RecipePost) => {
     withCredentials: true
   }).then(response => response.data)
   // error handled by react router
-}
-
-export const updateRecipe = async () => {
-  // TODO: UPDATE LIKES
-  return null
 }
 
 export const getIngredients = async () => {
